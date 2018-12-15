3fd728375bee748b4be6475c8fd2731f5929e9d5
jar100 <jar10038@gmail.com>
2018-12-15 14:04:03 +0900

    Step2 리펙토링및 수동테스트에서 발견한 오류수정

--- src/test/java/codesquad/web/QuestionAcceptanceTest.java
+++ src/test/java/codesquad/web/QuestionAcceptanceTest.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,7 +43,7 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
                 .build();
 
         ResponseEntity<String> response = basicAuthTemplate().postForEntity("/questions", request, String.class);
-        log.debug("body : {}",response.getBody());
+        log.debug("body : {}", response.getBody());
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
         softly.assertThat(questionRepository.findByTitle(title).isPresent()).isTrue();
         softly.assertThat(response.getHeaders().getLocation().getPath()).startsWith("/");
@@ -80,49 +81,52 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
-    public void update() throws  Exception {
-        HttpEntity<MultiValueMap<String, Object>> request = makeUpdateRequest();
-        ResponseEntity<String> response = basicAuthTemplate().postForEntity(String.format("/questions/%d", defaltQuestion().getId()), request, String.class);
+    public void update() throws Exception {
+        ResponseEntity<String> response = getUpdateResponse(basicAuthTemplate());
 
-        log.debug("body : {}",response.getBody());
+        log.debug("body : {}", response.getBody());
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
-        softly.assertThat(response.getHeaders().getLocation().getPath()).startsWith(String.format("/questions/%d", defaltQuestion().getId()));
+        softly.assertThat(response.getHeaders().getLocation().getPath()).startsWith(defaltQuestion().generateUrl());
     }
 
     @Test
-    public void update_no() throws  Exception {
-        HttpEntity<MultiValueMap<String, Object>> request = makeUpdateRequest();
-        ResponseEntity<String> response = template().postForEntity(defaltQuestion().generateUrl(), request, String.class);
+    public void update_no() throws Exception {
+        ResponseEntity<String> response = getUpdateResponse(template());
 
-        log.debug("body : {}",response.getBody());
+        log.debug("body : {}", response.getBody());
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
 
     }
 
-    private HttpEntity<MultiValueMap<String, Object>> makeUpdateRequest() {
-        return HtmlFormDataBuilder
+    private ResponseEntity<String> getUpdateResponse(TestRestTemplate template) {
+        HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder
                 .urlEncodedForm()
                 .put()
                 .addParameter("title", "하하하")
                 .addParameter("contents", "test dydydy")
                 .build();
+        return template.postForEntity(defaltQuestion().generateUrl(), request, String.class);
     }
 
+
     @Test
-    public void deleted() throws  Exception {
-        HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm().delete().build();
-        ResponseEntity<String> response = basicAuthTemplate().postForEntity(defaltQuestion().generateUrl(),request,String.class);
-        log.debug("body : {}",response.getBody());
+    public void deleted() throws Exception {
+        ResponseEntity<String> response = getStringResponseEntity(basicAuthTemplate());
+        log.debug("body : {}", response.getBody());
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
         softly.assertThat(response.getHeaders().getLocation().getPath()).startsWith("/");
 
     }
 
     @Test
-    public void deleted_no() throws  Exception {
-        HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm().delete().build();
-        ResponseEntity<String> response = template().postForEntity(defaltQuestion().generateUrl(),request,String.class);
-        log.debug("body : {}",response.getBody());
+    public void deleted_no() throws Exception {
+        ResponseEntity<String> response = getStringResponseEntity(template());
+        log.debug("body : {}", response.getBody());
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    private ResponseEntity<String> getStringResponseEntity(TestRestTemplate template) {
+        HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm().delete().build();
+        return template.postForEntity(defaltQuestion().generateUrl(), request, String.class);
     }
 }
